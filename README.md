d7ac5648aa3c30392438773aaf6060dc5cffab15
rdspoons <rdspoons@hotmail.com>
2017-01-07 00:02:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+fcc client information parser microservice.
+Returns informtion about the client in JSON format.
+The data returned is the client ip, language and software.
+
+example:
+{ip: 127.0.0.1,"language":"en-US","software":"Windows NT 6.0; WOW64; rv:50.0"}
---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require( 'http' ) ,
+    port = 8080;
+
+var server = http.createServer( function( req, res ) {
+	var ip = req.connection.remoteAddress ,
+	    lang = req.headers[ "accept-language" ] ,
+	    platform = req.headers[ "user-agent" ] ;
+
+	ip = ip.toString( ).replace( /[\s\S]+ffff:([\s\S]+)/ , "$1" ) ;
+	// Restrict languag to first description per fcc example.
+	lang = lang.split(",")[ 0 ] ;
+	// Restrict software to platform per fcc example.
+	platform = platform.replace( /.+\((.+)\).+/ , "$1" ) ;
+
+	var json = { }
+	json["ip"] = ip ;
+	json["language"] = lang ,
+	json["software"] = platform ;
+
+	res.end( JSON.stringify( json ) ) ;
+});
+
+server.listen( process.env.port || Number( port ) ) ;
